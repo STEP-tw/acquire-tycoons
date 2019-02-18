d6483b0cc63b410a2dfce129c1c421983242ca4f
Dheeraj Pundir <44019618+dheeraj-p@users.noreply.github.com>
2019-02-18 17:08:17 +0530

    [#4] Arnab/Dheeraj - Improve UI for homepage.
    What changes have you made?
    
    * Add home.css to style homepage.
    * Rename main.js to home.js
    
    Why do we need this change?
    
    * To improve UI

---
+++ public/scripts/home.js
@@ -0,0 +1,89 @@
+const setDisplay = function(elementId, display) {
+  const element = document.getElementById(elementId);
+  element.style.display = display;
+};
+
+const joinGame = function() {
+  const gameID = document.getElementById('game-id-field').value;
+  const playerName = document.getElementById('player-name').value;
+
+  const body = JSON.stringify({ gameID, playerName });
+  const requestData = {
+    method: 'POST',
+    headers: {
+      'content-type': 'application/json'
+    },
+    body
+  };
+
+  fetch('/join-game', requestData)
+    .then(response => response.json())
+    .then(resData => {
+      const { error, message } = resData;
+      if (error) {
+        const errorMessageBox = document.getElementById('join-error-box');
+        errorMessageBox.innerText = message;
+        return;
+      }
+      setDisplay('join-game-form', 'none');
+      setDisplay('waiting-area', 'flex');
+      document.getElementById('game-id').innerText = `Game ID: ${gameId}`;
+    });
+};
+
+const fetchGameId = function(requestData) {
+  fetch('/host-game', requestData)
+    .then(response => response.json())
+    .then(resData => {
+      const { gameId } = resData;
+      setDisplay('create-game-form', 'none');
+      setDisplay('waiting-area', 'flex');
+      document.getElementById('game-id').innerText = `Game ID: ${gameId}`;
+    });
+};
+
+const hostGame = function() {
+  let host = document.getElementById('host-name').value;
+  let totalPlayers = document.getElementById('total-players').value;
+  if (host) {
+    let requestData = {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ host, totalPlayers })
+    };
+    fetchGameId(requestData);
+    return;
+  }
+};
+
+const initialize = function() {
+  setDisplay('create-game-form', 'none');
+  setDisplay('join-game-form', 'none');
+  setDisplay('waiting-area', 'none');
+
+  const createGameButton = document.getElementById('create-game-btn');
+
+  createGameButton.onclick = function() {
+    setDisplay('game-options-container', 'none');
+    setDisplay('create-game-form', 'flex');
+  };
+
+  const joinGameButton = document.getElementById('join-game-btn');
+
+  joinGameButton.onclick = function() {
+    setDisplay('game-options-container', 'none');
+    setDisplay('join-game-form', 'flex');
+  };
+
+  //For join game form
+  const joinGameButtonPrime = document.getElementById('join-game-button-prime');
+  joinGameButtonPrime.onclick = joinGame;
+
+  //For create game form
+  const createGameButtonPrime = document.getElementById(
+    'create-game-button-prime'
+  );
+  createGameButtonPrime.onclick = hostGame;
+};
+
+window.onload = initialize;
