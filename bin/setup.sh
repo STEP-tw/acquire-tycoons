887c998c9e20193751127b2eecad6999fe427514
Dheeraj Pundir <44019618+dheeraj-p@users.noreply.github.com>
2019-02-24 14:48:28 +0530

    Dheeraj/Sai - Modify editor settings

--- bin/setup.sh
+++ bin/setup.sh
@@ -2,5 +2,7 @@
 
 cp .github/git_precommit.sh .git/hooks/pre-commit
 chmod +x .git/hooks/pre-commit
+cp .github/git_precommit.sh .git/hooks/pre-push
+chmod +x .git/hooks/pre-push
 git config --local commit.template '.github/git_commit_template'
 npm install
