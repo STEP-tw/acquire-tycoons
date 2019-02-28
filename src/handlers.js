4d69f01b244515b109cdf3484e1d7bc998a3883d
Swagata Chakraborty <44020073+swagatachakraborty@users.noreply.github.com>
2019-02-28 09:42:55 +0530

    Swagata/Sai - fix eslint errors.
    What changes have you made?
    
    * fix eslint errors

--- src/handlers.js
+++ src/handlers.js
@@ -1,8 +1,8 @@
-const Game = require("./models/game.js");
-const Player = require("./models/player.js");
-const { initializeGame } = require("./util.js");
-const ActivityLog = require("./models/activity_log");
-const { validateGameSession, validateTurn } = require("./validators");
+const Game = require('./models/game.js');
+const Player = require('./models/player.js');
+const { initializeGame } = require('./util.js');
+const ActivityLog = require('./models/activity_log');
+const { validateGameSession, validateTurn } = require('./validators');
 // const requiredFunctionality = require("../helpers/main.js")
 // .merger_big_small_test;
 
@@ -14,8 +14,8 @@ const hostGame = function(req, res) {
   game.addPlayer(hostPlayer);
   res.app.gameManager.addGame(game);
   let gameId = res.app.gameManager.getLatestId();
-  res.cookie("gameId", `${gameId}`);
-  res.cookie("playerId", `${playerId}`);
+  res.cookie('gameId', `${gameId}`);
+  res.cookie('playerId', `${playerId}`);
   res.send({ gameId });
 };
 
@@ -31,16 +31,16 @@ const joinGame = function(req, res) {
   const game = gameManager.getGameById(gameID);
 
   if (game.isFull()) {
-    res.send({ error: true, message: "Sorry! Game has already started." });
+    res.send({ error: true, message: 'Sorry! Game has already started.' });
     return;
   }
 
   let playerId = game.getNextPlayerId();
   let player = new Player(playerName, playerId);
   game.addPlayer(player);
-  res.cookie("gameId", `${gameID}`);
-  res.cookie("playerId", `${playerId}`);
-  res.send({ error: false, message: "" });
+  res.cookie('gameId', `${gameID}`);
+  res.cookie('playerId', `${playerId}`);
+  res.send({ error: false, message: '' });
   if (game.isFull()) {
     // gameManager.games[gameID] = requiredFunctionality();
     initializeGame(game);
@@ -58,16 +58,16 @@ const getGameStatus = function(req, res) {
 const renderGamePage = function(req, res) {
   const { gameId } = req.cookies;
   if (!res.app.gameManager.doesGameExist(gameId)) {
-    res.redirect("/");
+    res.redirect('/');
     return;
   }
   const game = res.app.gameManager.getGameById(gameId);
-  let message = "Waiting for other players to join";
+  let message = 'Waiting for other players to join';
   if (game.isStarted) {
-    message = "Loading game...";
+    message = 'Loading game...';
   }
 
-  res.render("game", { gameId, message });
+  res.render('game', { gameId, message });
 };
 
 const serveGameData = function(req, res) {
@@ -97,7 +97,7 @@ const buyStocks = function(req, res) {
   const { gameId } = req.cookies;
   const game = res.app.gameManager.getGameById(gameId);
   game.buyStocks(details);
-  res.send({ error: false, message: "" });
+  res.send({ error: false, message: '' });
 };
 
 module.exports = {
